e22ce9d171002f4635753b90617e77b930b31e43
LGiki <qq547221432@gmail.com>
2022-06-24 09:23:22 +0800

    Set url launch mode to external application

---
+++ lib/utils/url.dart
@@ -0,0 +1,12 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class Url {
+  static launchURL(String url) async {
+    Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
