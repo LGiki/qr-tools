850d6a5b05f6a3e92cf80248981e9cd3486cf394
LGiki <qq547221432@gmail.com>
2022-06-24 14:10:03 +0800

    Add phone number detail widget

---
+++ lib/widgets/barcode_detail/phone_number_detail.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:qr_tools/utils/url.dart';
+
+class PhoneNumberDetail extends StatelessWidget {
+  const PhoneNumberDetail({Key? key, required this.phoneNumberUrl}) : super(key: key);
+
+  final String phoneNumberUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    String phoneNumber = phoneNumberUrl.replaceAll(RegExp('tel:[//]*'), "");
+
+    return Column(
+      children: <Widget>[
+        ListTile(
+          onTap: () {
+            Url.launchURL('tel:$phoneNumber');
+          },
+          leading: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              Icon(Icons.call),
+            ],
+          ),
+          title: Text('Call $phoneNumber'),
+        ),
+      ],
+    );
+  }
+}
